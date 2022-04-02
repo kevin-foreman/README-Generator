4bb40caf506ff2c38a59006e311a092019dfe7cc
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-02 12:07:09 -0500

    cleande up some code in generateMarkdown, App will run, but no output

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const readmeDataArgs = process.argv.slice(2);
-const fs = require('fs');
+// const readmeDataArgs = process.argv.slice(2);
+// const { fstat } = require("fs");
+// const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./src/generateMarkdown')
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 
 // TODO: Create an array of questions for this application
-const promptQuestions = readmeData => {
-    if (!readmeData.sections) {
-        readmeData.sections = [];
+const promptQuestions = () => {
+    
+    // if (!readmeData.sections) {
+    //     readmeData.sections = [];
     }
     console.log(`
     =================
@@ -19,29 +22,30 @@ const promptQuestions = readmeData => {
     {
         type: 'input',
         name: 'title',
-        message: 'What is the name of the project?'
+        message: 'What is the name of the project?',
         validate: nameInput => {
+            // console.log(nameInput);
         if (nameInput) {
             return true;
         } else {
             console.log("Please enter the name of the project");
             return false;
         }
-    };
-    };
+    }
+    },
     {
         type: 'input',
         name: 'description',
         message: 'Provide a brief description of the project (Required)',
         validate: nameInput => {
-        if (nameInpujt) => {
+        if (nameInput) {
             return true;
         } else {
             console.log('Please enter a brief description of your project');
             return false;
         };
-    };
-    };
+    }
+    },
     // '## Table of Contents',
     // '* [Installation](#installation)',
     // '* [Usage](#usage)',
@@ -53,53 +57,74 @@ const promptQuestions = readmeData => {
         type: 'input',
         name: 'installation',
         message: 'Provide instructions how to install your project (if necessary)'
-
+    },
+    {
         type: 'input',
         name: 'usage',
-        message: 'Provide instructions and examples for use.',
-                 'Add screenshots as necessary.',
-                 'To add a screenshot, create an `assets/images` folder in your repository',
-                 'and upload your screenshot to it. Then, using the relative filepath,',
-                 'add it to your README using the following syntax:',
-                 '![alt text](assets/images/screenshot.png)'
-    }
+        message: 'Provide instructions and examples for use. Add screenshots as necessary. To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath add it to your README using the following syntax: <![alt text](assets/images/screenshot.png)'
+    },
     {
         type: 'checkbox',
         name: 'license',
         choices: ["MIT", "Apache 2.0", "GPL", "GPLv3"],
-        }
+    },
     {
         type: 'input',
         name: 'contributors',
         message: 'Identify contributors here, if any'
-        default: false,
-    }
+        // default: false
+    },
     {
         type: 'input',
         name: 'tests',
         message: 'Enter any testing you have done in your project'
-    }
+    },
     {
         type: 'inpuit',
         name: 'questions',
         message: 'Enter your email address to add a way to contact you with questions'
     }
 ])
-};
+// };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return fs.writeToFile ('README.md')
+const writeFile = (fileName, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./utils/index.js', fileName, data, err => {
+            // if there is an error, reject the Promise and send the error
+            if (err) {
+                reject(err);
+                // return our of the function to stop the process
+                return;
+            }
+
+            // if all is well, resolve the promise
+            resolve({
+                ok: true,
+                message: 'File created'
+            });
+        });
+    });
 };
 
+promptQuestions()
+.then(readmeData => {
+    return generateMarkdown(readmeData);
+    })
+    .then(readmeData => {
+        return writeFile('Readme.md');
+    })
+console.log(readmeData);
+    
+
 // TODO: Create function to initialize the app
-function init() {
+// function init(generateMarkdown) {
+//     promptQuestions();
+// }
 
-}
 
 
-console.log(title, description);
-console.log(generateMarkdown(title, description));
+// console.log(generateMarkdown(title, description));
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,79 +1,103 @@
 const fs = require('fs');
-const { fstat } = require("fs");
-const generateMarkdown = require(g);
+
+
+// const { fstat } = require("fs");
+// const generateMarkdown = require();
 
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-    if(license != '') {
-    return false;
-    } else {
-        // ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
-    };
+// function renderLicenseBadge(License) {
+//     if(license != '') {
+//     return false;
+//     } else {
+//         ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+//     }
 
-};
+// }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-    if (license != '') {
-    } else {
-
-    }
-}
+// function renderLicenseLink(License) {
+    // if (license != '') {
+    // } else {
+    // return `
+    // ## License
+    // The license used for this project is [$(License)](#License)
+    // `
+    // }
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(License) {}
+
+// function renderLicense(License) {
+//     if(license != 'None') {
+//         } else {
+//             `
+//             The license used for this project is `
+
+//         }
+// }
 
 // TODO: Create a function to generate markdown for README
 
-function generateMarkdown(data) {
+const generateMarkdown = readMeArr => {
     return `
+    ${readMeArr
+        .map(({ title, description, installation, usage, license, contributors, tests, questions }) => {
+        return `
     # Title
-    ${title}
+    ${data.title.join}
 
     ## Description
-    ${description}
+    ${data.description}
 
     ## Table of Contents
     * [Installation](#installation)
     * [Usage](#usage)
-    * License
-    * Contributors
-    * Tests
-    * Questions   
-    ${tableOfContents}
+    * [License](#license)
+    * [Contributors](#contributors)
+    * [Tests](#tests)
+    * [Questions](#questions)   
+    ${data.tableOfContents.join}
 
     ## Installation
-    ${installation}
+    ${data.installation.join}
 
     ## Usage
-    ${usage}
+    ${data.usage}
 
     ## License
-    ${license}
+    ${data.license.join(', ')}
 
     ## Contributors
-    ${contributors}
+    ${data.contributors}
 
     ## Tests
-    ${tests}
+    ${data.tests}
     // Contact information (email address) of the developer
     ## Questions
-    ${questions}
+    ${data.questions}
+    }})
     `;
+})
+    .join('')}
+    `;   
 };
 
+module.exports = generateMarkdown 
 
-function renderLicense(license) {
-    if(license != 'None') {
-        } else {
 
-        }
-}
+// => {
+//     console.log(templateData);
+//     const { title, description, installation, usage, license, contributors, tests, questions } = readmeData;
+//     const generateFile = readmeArr => {
+//         if (!readmeArr) {
+//             return '';
+//         }
+//     }
+// }
 
-module.exports = { writeFile, copyFile };
-
-fstat.writeFile
+// fstat.writeFile
